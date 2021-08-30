ae282db21d8ec354ef6475e51f1d7409f6a02185
Djonathan Quadras <djonquadras@gmail.com>
2021-08-30 11:36:51 -0300

    Correções e atualizações.

--- Scripts uteis/Correção Dados Demanda.R
+++ Scripts uteis/Correção Dados Demanda.R
@@ -1,17 +1,6 @@
-source("Scripts uteis/Functions.R")
+library(openxlsx)
+
+demandas <- openxlsx::read.xlsx("D:/Google Drive/UFSC/Monografia/ChegadaOrdens.xlsx", detectDates = TRUE)
 
 
-demandas <- getDemanda("Dados/dados.xlsx")
-listasTecnicas <- getLista("Dados/dados.xlsx")
-
-#Pra ver
-
-tudoJunto <- merge(x = demandas, y = listOfMaterial, by.x = "Material", by.y = "Material")
-
-unique(demanda$`Texto breve material`)
-
-data <- tibble(a = c(1,2,3),
-               b = c(1,2,3))
-
-data$c <- c(1,2,3)
-data$d <- c(NA)
+length(unique(demandas$productionorder))
